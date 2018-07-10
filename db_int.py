5f59ec3cd8dcafc964fe479f3f7e2257597692af
koechkevin <koechkevin92@gmail.com>
2018-07-10 10:27:27 +0300

    updated

--- db_int.py
+++ db_int.py
@@ -6,10 +6,13 @@ from functools import wraps
 app=Flask(__name__)
 app.config['SECRET_KEY']="koech"
 
-userID=[]          
+          
+
 @app.route('/',methods=['GET'])
 def home():
     return jsonify({"message":"Please login to continue"})
+
+
 @app.route('/register',methods=['POST'])
 def register():
     fname=request.get_json()["Fname"]
@@ -33,6 +36,8 @@ def register():
     finally:
         connection.close()
     return jsonify({"message": "Register successful"})
+
+
 @app.route('/login',methods=['POST'])
 def login():
     username=request.get_json()["username"]
@@ -49,7 +54,7 @@ def login():
                     cursor1.execute(sql1)
                     result = cursor1.fetchall()
                     for each in result:
-                        rawtoken.update({"username":str(each[1]),"exp":datetime.datetime.utcnow()+datetime.timedelta(minutes=5)})
+                        rawtoken.update({"user":"admin","username":str(each[1]),"exp":datetime.datetime.utcnow()+datetime.timedelta(minutes=15)})
                 except:
                     return jsonify({"message": "oops unsuccessful"})
             connection.commit()
@@ -58,7 +63,7 @@ def login():
         if len(rawtoken)==0:
             return jsonify({"message":"you are not an admin"})            
         token=jwt.encode(rawtoken,app.config['SECRET_KEY'])    
-        return jsonify({"Admintoken":token.decode('utf-8')})
+        return jsonify({"token":token.decode('utf-8')})
         
     else:
         try:
@@ -69,8 +74,8 @@ def login():
                     cursor1.execute(sql1)
                     result = cursor1.fetchall()
                     for each in result:
-                        rawtoken.update({"username":str(each[4]),"exp":datetime.datetime.utcnow()+datetime.timedelta(minutes=5)})
-                        userID.append(each[0])
+                        rawtoken.update({"ID":str(each[0]),"user":"normal","username":str(each[4]),"exp":datetime.datetime.utcnow()+datetime.timedelta(minutes=15)})
+                        
                 except:
                     return jsonify({"message": "oops unsuccessful"})      
             connection.commit()
@@ -80,34 +85,48 @@ def login():
         return jsonify({"message":"Invalid Credentials"})
     token=jwt.encode(rawtoken,app.config['SECRET_KEY'])        
     return jsonify({"token":token.decode('utf-8')})
+
+
 def authorizeAdmin(t):
     @wraps(t)
     def decorated(*args,**kwargs):
-        if request.args.get('Admintoken')=='':
-            return jsonify({"Alert":'please login as admin'})
+        if request.args.get('token')=='':
+            return jsonify({"Alert":'Token is missing'})
         try:
-            jwt.decode(request.args.get('Admintoken'), app.config['SECRET_KEY'])
+            data=jwt.decode(request.args.get('token'), app.config['SECRET_KEY'])
+            
+            if data['user']=='normal':
+                return jsonify({"Alert":'Only admins can access this'})
         except:    
             return jsonify({"Alert":'please login again'})
         return t(*args,**kwargs)
     return decorated
+
+
 def authorizeUser(f):
     @wraps(f)
     def decorated(*args,**kwargs):
         if request.args.get('token')=='':
             return jsonify({"Alert":'please login as user'})
         try:
-            jwt.decode(request.args.get('token'), app.config['SECRET_KEY'])
+            data=jwt.decode(request.args.get('token'), app.config['SECRET_KEY'])
+            if data['user']=='admin':
+                return jsonify({"Alert":"utilize admin rights"})
+            userID.append(data['ID'])
         except:
             return jsonify({"Alert":'please login again'})
         return f(*args,**kwargs)
     return decorated
+
+
 @app.route('/post_comment',methods=['POST'])
 @authorizeUser
 def post_comment():
     comment=request.get_json()["comment"]
     timestamp=datetime.datetime.today().strftime('%Y%m%d%H%M%S')
-    writer=str(userID[len(userID)-1])
+    #writer=''str(userID[len(userID)-1])
+    data=jwt.decode(request.args.get('token'), app.config['SECRET_KEY'])
+    writer=data['ID']
     try:
         connection = pymysql.connect(host='localhost',user='root',password='',db='andela')
         with connection.cursor() as cursor:
@@ -120,14 +139,18 @@ def post_comment():
     finally:
             connection.close()
     return jsonify({"message": "successful comments"})
+
+
 @app.route('/view_comments',methods=['GET'])
 @authorizeUser
 def view_comments():
     output={}
+    data=jwt.decode(request.args.get('token'), app.config['SECRET_KEY'])
+    myID=data['ID']    
     try:
         connection = pymysql.connect(host='localhost',user='root',password='',db='andela')
         with connection.cursor() as cursor:
-            sql = "SELECT * FROM `comments` WHERE `ID` = "+str(userID[len(userID)-1])+";"
+            sql = "SELECT * FROM `comments` WHERE `ID` = "+myID+";"
             try:
                 cursor.execute(sql)
                 result=cursor.fetchall()
@@ -139,22 +162,25 @@ def view_comments():
     finally:
         connection.close()
     return jsonify(output)
+
+
 @app.route('/account',methods=['GET'])
 @authorizeUser
 def account():
-    myID=''
+    data=jwt.decode(request.args.get('token'), app.config['SECRET_KEY'])
+    myID=data['ID']    
     Name=''
     Email=''
     username=''
     try:
         connection = pymysql.connect(host='localhost',user='root',password='',db='andela')
         with connection.cursor() as cursor:
-            sql = "SELECT * FROM `users` WHERE `ID` = "+str(userID[len(userID)-1])+""
+            sql = "SELECT * FROM `users` WHERE `ID` = "+myID+""
             try:
                 cursor.execute(sql)
                 result=cursor.fetchall()
                 for each in result:
-                    myID+=str(each[0])
+                    
                     Name+=str(each[1])+' '+str(each[2])
                     Email+=str(each[3])
                     username+=str(each[4])
@@ -164,16 +190,20 @@ def account():
     finally:
         connection.close()
     return jsonify({"username":username,"Name":Name,"Email":Email,"userID":myID})
+
+
 @app.route('/delete_comment/<int:commentID>',methods=['DELETE'])
 @authorizeUser
 def delete_comment(commentID):
+    data=jwt.decode(request.args.get('token'), app.config['SECRET_KEY'])
+    myID=data['ID']    
     try:
         connection = pymysql.connect(host='localhost',user='root',password='',db='andela')
         with connection.cursor() as cursor:
-            sql="DELETE FROM `comments` WHERE `comments`.`commentID` = "+str(commentID)+" and `comments`.`ID`="+str(userID[len(userID)-1])+""
+            sql="DELETE FROM `comments` WHERE `comments`.`commentID` = "+str(commentID)+" and `comments`.`ID`="+myID+""
             
             try:
-                cursor.execute("SELECT * FROM `comments` WHERE `commentID` LIKE "+str(commentID)+" and `ID` LIKE "+str(userID[len(userID)-1])+" ")
+                cursor.execute("SELECT * FROM `comments` WHERE `commentID` LIKE "+str(commentID)+" and `ID` LIKE "+myID+" ")
                 result=cursor.fetchone()
                 if result is None:
                     return jsonify({"Alert":"You are not Authorized to delete this comment"})
@@ -185,6 +215,8 @@ def delete_comment(commentID):
     finally:
         connection.close()
     return jsonify({"message": "successfully deleted,you cannot undo this"})
+
+
 @app.route('/allcomments/<int:commentID>',methods=['DELETE'])
 @authorizeAdmin
 def delete_commentsByAdmin(commentID):
@@ -200,6 +232,8 @@ def delete_commentsByAdmin(commentID):
     finally:
         connection.close()           
     return jsonify({"message": "successful"})
+
+
 @app.route('/view_all',methods=['GET'])
 @authorizeAdmin
 def view_commentsByAdmin():
